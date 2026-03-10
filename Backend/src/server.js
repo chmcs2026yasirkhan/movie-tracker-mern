51f937b0699a875c60562a81144b99442404f78d
Yasir <chmcs2026yasirkhan@gmail.com>
2026-03-10 07:07:34 +0530

    Updated API URL and CORS for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"],
 }));
