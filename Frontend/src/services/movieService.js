9a07a62857922280bc6a419a80d449458d35a6bc
Yasir <chmcs2026yasirkhan@gmail.com>
2026-03-10 07:05:11 +0530

    Updated API URL to Render

--- Frontend/src/services/movieService.js
+++ Frontend/src/services/movieService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/movies";
+const API_BASE_URL = "https://movie-tracker-mern.onrender.com/api/movies";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
